0a476a134915bad0a9d56a7602773519093c5b7d
Pavel Vasilyev <pavel.vas@mail.ru>
2023-05-05 20:03:34 +0300

    Add BaseSchema

---
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -0,0 +1,29 @@
+package hexlet.code.schemas;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Predicate;
+
+public abstract class BaseSchema {
+    private Map<String, Predicate<Object>> validityChecks;
+
+    public BaseSchema() {
+        this.validityChecks = new LinkedHashMap<>();
+        this.typeCheck();
+    }
+
+    abstract void typeCheck();
+
+    public void addValidityCheck(String checkName, Predicate<Object> method) {
+        validityChecks.put(checkName, method);
+    }
+
+    public boolean isValid(Object content) {
+        for (String check: validityChecks.keySet()) {
+            if  (!validityChecks.get(check).test(content)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
