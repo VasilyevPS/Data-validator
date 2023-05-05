26235059344451792b37c7868345bf2fe3d8d774
Pavel Vasilyev <pavel.vas@mail.ru>
2023-05-05 20:05:26 +0300

    Refactor schemas

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,40 +1,22 @@
 package hexlet.code.schemas;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.function.Predicate;
 
-public class NumberSchema {
-    private Map<String, Predicate<Object>> validityChecks;
+public final class NumberSchema extends BaseSchema {
 
-    public NumberSchema() {
-        this.validityChecks = new LinkedHashMap<>();
-        this.typeCheck();
-    }
-
+    @Override
     void typeCheck() {
-        validityChecks.put("typeCheck", (content -> content instanceof Integer || content == null));
+        addValidityCheck("typeCheck", (content -> content instanceof Integer || content == null));
     }
 
     public void required() {
-        validityChecks.put("required", (Objects::nonNull));
+        addValidityCheck("required", (content -> content != null));
     }
 
     public void positive() {
-        validityChecks.put("positive", (content -> content == null || (int) content > 0));
+        addValidityCheck("positive", (content -> content == null || (int) content > 0));
     }
 
     public void range(int lowLimit, int highLimit) {
-        validityChecks.put("range", (content -> lowLimit <= (int) content && (int) content <= highLimit));
-    }
-
-    public boolean isValid(Object content) {
-        for (String check: validityChecks.keySet()) {
-            if  (!validityChecks.get(check).test(content)) {
-                return false;
-            }
-        }
-        return true;
+        addValidityCheck("range", (content -> lowLimit <= (int) content && (int) content <= highLimit));
     }
 }
--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,39 +1,22 @@
 package hexlet.code.schemas;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.function.Predicate;
 
-public class StringSchema {
-    private Map<String, Predicate<Object>> validityChecks;
+public final class StringSchema extends BaseSchema {
 
-    public StringSchema() {
-        this.validityChecks = new LinkedHashMap<>();
-        this.typeCheck();
-    }
-
+    @Override
     void typeCheck() {
-        validityChecks.put("typeCheck", (content -> content instanceof String || content == null));
+        addValidityCheck("typeCheck", (content -> content instanceof String || content == null));
     }
 
     public void required() {
-        validityChecks.put("required", (content -> content != null && !content.equals("")));
+        addValidityCheck("required", (content -> content != null && !content.equals("")));
     }
 
     public void minLength(int limit) {
-        validityChecks.put("minLength", (content -> limit <= String.valueOf(content).length()));
+        addValidityCheck("minLength", (content -> limit <= String.valueOf(content).length()));
     }
 
     public void contains(String text) {
-        validityChecks.put("contains", (content -> String.valueOf(content).contains(text)));
-    }
-
-    public boolean isValid(Object content) {
-        for (String check: validityChecks.keySet()) {
-            if  (!validityChecks.get(check).test(content)) {
-                return false;
-            }
-        }
-        return true;
+        addValidityCheck("contains", (content -> String.valueOf(content).contains(text)));
     }
 }
