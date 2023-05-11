b245bb1abadafed604111be93503cd980a524177
Pavel Vasilyev <pavel.vas@mail.ru>
2023-05-11 16:41:49 +0300

    Add shape method

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -21,7 +21,19 @@ public class MapSchema extends BaseSchema {
     }
 
     public MapSchema shape(Map<String, BaseSchema> schemas) {
-
+        addValidityCheck("shape ",
+                content -> {
+                    Map<?, ?> contentMap = (Map<?, ?>) content;
+                    for (Map.Entry<String, BaseSchema> entry: schemas.entrySet()) {
+                        String parameterName = entry.getKey();
+                        BaseSchema currentSchema = entry.getValue();
+                        if (!currentSchema.isValid(contentMap.get(parameterName))) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+        );
         return this;
     }
 }
