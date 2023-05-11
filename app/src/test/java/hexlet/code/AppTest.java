2d07fb1d065d91bf119e9d179c6af3e70468cd8b
Pavel Vasilyev <pavel.vas@mail.ru>
2023-05-11 16:39:14 +0300

    Add tests for shape method

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import hexlet.code.schemas.BaseSchema;
 import hexlet.code.schemas.MapSchema;
 import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
@@ -87,5 +88,50 @@ final class AppTest {
         assertThat(schema.isValid(testMap)).isTrue();
         testMap.put("key3", "value3");
         assertThat(schema.isValid(testMap)).isFalse();
+    }
+
+    @Test
+    void mapSchemaShapeTest() {
+        Validator v = new Validator();
+        MapSchema schema = v.map();
+        Map<String, BaseSchema> schemas = new HashMap<>();
+        schemas.put("name", v.string().required());
+        schemas.put("age", v.number().positive());
+        schema.shape(schemas);
+
+        Map<String, Object> human1 = new HashMap<>();
+        human1.put("name", "Kolya");
+        human1.put("age", 100);
+        assertThat(schema.isValid(human1)).isTrue();
+
+        Map<String, Object> human2 = new HashMap<>();
+        human2.put("name", "Maya");
+        human2.put("age", null);
+        assertThat(schema.isValid(human2)).isTrue();
+
+        Map<String, Object> human3 = new HashMap<>();
+        human3.put("name", "");
+        human3.put("age", null);
+        assertThat(schema.isValid(human3)).isFalse();
+
+        Map<String, Object> human4 = new HashMap<>();
+        human4.put("name", "Valya");
+        human4.put("age", -5);
+        assertThat(schema.isValid(human4)).isFalse();
+
+        schemas.put("name", v.string().required().contains("Ma"));
+        schemas.put("age", v.number().positive());
+        schema.shape(schemas);
+
+        assertThat(schema.isValid(human1)).isFalse();
+        assertThat(schema.isValid(human2)).isTrue();
+
+        schemas.put("name", v.string().required());
+        schemas.put("age", v.number().positive().required());
+        schema.shape(schemas);
+
+        assertThat(schema.isValid(human1)).isTrue();
+        assertThat(schema.isValid(human2)).isFalse();
+
     }
 }
